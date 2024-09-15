2f3f56ab1791087c228a8ca86ac6642cbb3400c9
myhai05 <mmishk0501@gmail.com>
2024-09-15 14:43:17 +0200

    cpors policy

--- socket.js
+++ socket.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONEND_CORS_URL, // Remplacez par votre domaine front-end 
-    credentials: true, // Permet l'envoi des cookies à travers les domaines
+    credentials: false, // Permet l'envoi des cookies à travers les domaines
     allowedHeaders: ['sessionId', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Headers autorisés
     exposedHeaders: ['sessionId'], // Headers exposés
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Méthodes HTTP autorisées
